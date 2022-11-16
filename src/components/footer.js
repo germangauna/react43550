1862d108d81af124724a9979b025c03321688a13
German Gauna <germangauna88@gmail.com>
2022-11-16 19:53:06 -0300

    se a creado la carpeta componets

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import react from 'react'// este se puede sacar, solo si es necesario
+//rafce: te crea todo el codigo necesario
+
+const footer = () => {
+    return (
+        <div>footer</div>
+    )
+}                                   
+
+export default footer
